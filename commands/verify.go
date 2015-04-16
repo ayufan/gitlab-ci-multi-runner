fc4488dabaab3c9e8bed4d6b3cf5fcca897c3f1b
Kamil Trzcinski <ayufan@ayufan.eu>
2015-04-16 16:57:18 +0200

    Added option to verify registered runners and possible delete the removed ones

---
+++ commands/verify.go
@@ -0,0 +1,63 @@
+package commands
+
+import (
+	"github.com/codegangsta/cli"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/ayufan/gitlab-ci-multi-runner/common"
+)
+
+func runVerify(c *cli.Context) {
+	config := &common.Config{}
+	err := config.LoadConfig(c.String("config"))
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	// verify if runner exist
+	runners := []*common.RunnerConfig{}
+	for _, runner := range config.Runners {
+		if common.VerifyRunner(runner.URL, runner.Token) {
+			runners = append(runners, runner)
+		}
+	}
+
+	if !c.Bool("delete") {
+		return
+	}
+
+	// check if anything changed
+	if len(config.Runners) == len(runners) {
+		return
+	}
+
+	config.Runners = runners
+
+	// save config file
+	err = config.SaveConfig(c.String("config"))
+	if err != nil {
+		log.Fatalln("Failed to update", c.String("config"), err)
+	}
+	log.Println("Updated", c.String("config"))
+}
+
+func init() {
+	common.RegisterCommand(cli.Command{
+		Name:   "verify",
+		Usage:  "verify all registered runners",
+		Action: runVerify,
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:   "c, config",
+				Value:  "config.toml",
+				Usage:  "Config file",
+				EnvVar: "CONFIG_FILE",
+			},
+			cli.BoolFlag{
+				Name:  "delete",
+				Usage: "Delete no longer existing runners?",
+			},
+		},
+	})
+}
