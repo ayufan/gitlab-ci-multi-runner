73b2dc53e4b215a7300812b6c95a1ea008b2a2a3
Aurélien Thieriot <aurelien@scalar.is>
2015-08-27 15:54:19 +0100

    Add first few tests. Starting with helpers

---
+++ helpers/docker_test.go
@@ -0,0 +1,50 @@
+package helpers
+
+import (
+	"testing"
+)
+
+func TestSplitDockerImageName(t *testing.T) {
+
+	remote, image := SplitDockerImageName("tutum.co/user/ubuntu")
+	expectedRemote := "tutum.co"
+	expectedImage := "user/ubuntu"
+
+	if remote != expectedRemote {
+		t.Error("Expected ", expectedRemote, ", got ", remote)
+	}
+
+	if image != expectedImage {
+		t.Error("Expected ", expectedImage, ", got ", image)
+	}
+}
+
+func TestSplitDefaultDockerImageName(t *testing.T) {
+
+	remote, image := SplitDockerImageName("user/ubuntu")
+	expectedRemote := "docker.io"
+	expectedImage := "user/ubuntu"
+
+	if remote != expectedRemote {
+		t.Error("Expected ", expectedRemote, ", got ", remote)
+	}
+
+	if image != expectedImage {
+		t.Error("Expected ", expectedImage, ", got ", image)
+	}
+}
+
+func TestSplitDefaultIndexDockerImageName(t *testing.T) {
+
+	remote, image := SplitDockerImageName("index.docker.io/user/ubuntu")
+	expectedRemote := "docker.io"
+	expectedImage := "user/ubuntu"
+
+	if remote != expectedRemote {
+		t.Error("Expected ", expectedRemote, ", got ", remote)
+	}
+
+	if image != expectedImage {
+		t.Error("Expected ", expectedImage, ", got ", image)
+	}
+}
