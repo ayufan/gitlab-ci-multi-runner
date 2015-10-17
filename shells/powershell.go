0271232872caf0ebf0224e04511de84e24bff578
Sebastian Neumann <basti.megamorf+gitlab-com@googlemail.com>
2015-10-17 16:21:54 +0000

    Added a missing set of braces in line 33 and cleaned up the code that checks if the cmdlet Remove-Item2 from the NTFSSecurity module is available.

--- shells/powershell.go
+++ shells/powershell.go
@@ -29,11 +29,10 @@ func (b *PowerShell) writeCommandChecked(w io.Writer, format string, args ...int
 
 func (b *PowerShell) writeCloneCmd(w io.Writer, build *common.Build, dir string) {
 	b.writeCommand(w, "echo \"Cloning repository...\"")
-	b.writeCommand(w, "$NTFSSecurityRemoveItem2 = Get-Command Remove-Item2 -ErrorAction SilentlyContinue")
-	b.writeCommand(w, "$HasNTFSSecurityRemoveItem2 = ($NTFSSecurityRemoveItem2 -ne $null) -and ($NTFSSecurityRemoveItem2.ModuleName -eq \"NTFSSecurity\")")
-	b.writeCommand(w, "if($HasNTFSSecurityRemoveItem2 -and Test-Path \"%s\") {", dir)
+	b.writeCommand(w, "Import-Module -Name NTFSSecurity -ErrorAction SilentlyContinue")
+	b.writeCommand(w, "if( (Get-Command -Name Remove-Item2 -Module NTFSSecurity -ErrorAction SilentlyContinue) -and (Test-Path \"%s\") ) {", dir)
 	b.writeCommandChecked(w, "Remove-Item2 -Force -Recurse \"%s\"", dir)
-	b.writeCommand(w, "} elseif(Test-Path \"%s\") {")
+	b.writeCommand(w, "} elseif(Test-Path \"%s\") {", dir)
 	b.writeCommandChecked(w, "Remove-Item -Force -Recurse \"%s\"", dir)
 	b.writeCommand(w, "}")
 	b.writeCommandChecked(w, "(Test-Path \"%s\") -or (New-Item \"%s\" -ItemType \"directory\" )", dir, dir)
