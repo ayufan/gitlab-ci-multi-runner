8d5f04251263c9c1679d7604a5545396c987085a
Kamil Trzcinski <ayufan@ayufan.eu>
2015-10-21 09:22:34 +0200

    Revert "Fix tags handling when using git fetch: fetch all tags and prune the old ones"
    This reverts commit 525bd5145bd5c643e615cc3fdbcbcfd77552ff87.
    
    Conflicts:
    	CHANGELOG.md

--- shells/cmd.go
+++ shells/cmd.go
@@ -43,7 +43,7 @@ func (b *CmdShell) writeFetchCmd(w io.Writer, build *common.Build, dir string) {
 	b.writeCommandChecked(w, "git clean -ffdx")
 	b.writeCommandChecked(w, "git reset --hard > NUL")
 	b.writeCommandChecked(w, "git remote set-url origin \"%s\"", build.RepoURL)
-	b.writeCommandChecked(w, "git fetch origin --tags -p")
+	b.writeCommandChecked(w, "git fetch origin")
 	b.writeCommand(w, ") ELSE (")
 	b.writeCloneCmd(w, build, dir)
 	b.writeCommand(w, ")")
