2c72c16ed83faabf2a4817576b443292eb104423
Kamil Trzcinski <ayufan@ayufan.eu>
2015-11-15 22:44:54 +0100

    Add command to find and archive the files

---
+++ commands/archive.go
@@ -0,0 +1,189 @@
+package commands
+
+import (
+	"bufio"
+	"bytes"
+	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/common"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
+)
+
+type ArchiveCommand struct {
+	Paths     []string `long:"path" description:"Add paths to archive"`
+	Untracked bool     `long:"untracked" description:"Add git untracked files"`
+	Output    string   `long:"output" description:"The filepath to output file"`
+	Silent    bool     `long:"silent" description:"Suppress archiving ouput"`
+
+	wd    string
+	files map[string]os.FileInfo
+}
+
+func (c *ArchiveCommand) isChanged(modTime time.Time) bool {
+	for _, info := range c.files {
+		if modTime.Before(info.ModTime()) {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *ArchiveCommand) sortedFiles() []string {
+	files := make([]string, len(c.files))
+
+	i := 0
+	for file := range c.files {
+		files[i] = file
+		i++
+	}
+
+	sort.Strings(files)
+	return files
+}
+
+func (c *ArchiveCommand) add(path string, info os.FileInfo) (err error) {
+	if info == nil {
+		info, err = os.Lstat(path)
+	}
+	if err == nil {
+		c.files[path] = info
+	} else if os.IsNotExist(err) {
+		logrus.Warningln("File", path, "doesn't exist")
+		err = nil
+	}
+	return
+}
+
+func (c *ArchiveCommand) process(match string) error {
+	absolute, err := filepath.Abs(match)
+	if err != nil {
+		return err
+	}
+
+	relative, err := filepath.Rel(c.wd, absolute)
+	if err != nil {
+		return err
+	}
+
+	// store relative path if points to current working directory
+	if strings.HasPrefix(relative, ".."+string(filepath.Separator)) {
+		return c.add(absolute, nil)
+	} else {
+		return c.add(relative, nil)
+	}
+}
+
+func (c *ArchiveCommand) processPaths() {
+	for _, path := range c.Paths {
+		matches, err := filepath.Glob(path)
+		if err != nil {
+			logrus.Warningln(err)
+			continue
+		}
+
+		for _, match := range matches {
+			err := filepath.Walk(match, func(path string, info os.FileInfo, err error) error {
+				return c.process(path)
+			})
+			if err != nil {
+				logrus.Warningln("Walking", match, err)
+			}
+		}
+	}
+}
+
+func (c *ArchiveCommand) processUntracked() {
+	if !c.Untracked {
+		return
+	}
+
+	var output bytes.Buffer
+	cmd := exec.Command("git", "ls-files", "-o")
+	cmd.Env = os.Environ()
+	cmd.Stdout = &output
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err == nil {
+		reader := bufio.NewReader(&output)
+		for {
+			line, _, err := reader.ReadLine()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				logrus.Warningln(err)
+				break
+			}
+			c.process(string(line))
+		}
+	} else {
+		logrus.Warningln(err)
+	}
+}
+
+func (c *ArchiveCommand) archive() {
+	if len(c.files) == 0 {
+		logrus.Infoln("No files to archive.")
+		return
+	}
+
+	logrus.Infoln("Creating archive", c.Output, "...")
+	var files bytes.Buffer
+	for _, file := range c.sortedFiles() {
+		files.WriteString(string(file) + "\n")
+	}
+
+	flags := "-zcPv"
+	if c.Silent {
+		flags = "-zcP"
+	}
+
+	cmd := exec.Command("tar", flags, "-T", "-", "-f", c.Output)
+	cmd.Env = os.Environ()
+	cmd.Stdin = &files
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		logrus.Fatalln("Failed to create archive", err)
+	}
+	logrus.Infoln("Done!")
+}
+
+func (c *ArchiveCommand) Execute(context *cli.Context) {
+	wd, err := os.Getwd()
+	if err != nil {
+		logrus.Fatalln("Failed to get current working directory", err)
+	}
+	if c.Output == "" {
+		logrus.Fatalln("Missing archive file name!")
+	}
+
+	c.wd = wd
+	c.files = make(map[string]os.FileInfo)
+
+	c.processPaths()
+	c.processUntracked()
+
+	ai, err := os.Stat(c.Output)
+	if err != nil && !os.IsNotExist(err) {
+		logrus.Fatalln("Failed to verify archive", c.Output, err)
+	}
+	if ai != nil {
+		if !c.isChanged(ai.ModTime()) {
+			logrus.Infoln("Archive is up to date!")
+			return
+		}
+	}
+
+	c.archive()
+}
+
+func init() {
+	common.RegisterCommand2("archive", "find and archive files (internal)", &ArchiveCommand{})
+}
