2a2494dd746542a4e19a69c9cc80d27b8064ef69
Benjamin Pannell <admin@sierrasoftworks.com>
2015-11-25 08:14:41 +0200

    feat: Give runner the ability to extract archives
    Archives are decompressed relative to the current working directory (the
    same way archiving works) and expected to be in tar+gzip format.
    
    See #298 for motivation/discussion
    /cc @ayufan

---
+++ commands/extract_test.go
@@ -0,0 +1,51 @@
+package commands
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/EMSSConsulting/Thargo"
+	"github.com/stretchr/testify/assert"
+)
+
+func createExtractCommand(t *testing.T) *ExtractCommand {
+	err := os.Chdir(filepath.Join(currentDir, ".."))
+	assert.NoError(t, err)
+
+	return &ExtractCommand{
+		Input:  randomTempFile(t),
+		Silent: true,
+	}
+}
+
+func writeArchive(t *testing.T, c *ExtractCommand, target thargo.Target) {
+	options := *thargo.DefaultOptions
+	options.CreateIfMissing = true
+	archive, err := thargo.NewArchiveFile(c.Input, &options)
+	assert.NoError(t, err)
+	defer archive.Close()
+	assert.NoError(t, archive.Add(target))
+}
+
+func TestExtract(t *testing.T) {
+	c := createExtractCommand(t)
+	writeArchive(t, c, &thargo.StringTarget{
+		Name:    "test.txt",
+		Content: "test file",
+	})
+	defer os.Remove(c.Input)
+	
+	c.Execute(nil)
+	
+	stat, err := os.Stat("test.txt")
+	assert.False(t, os.IsNotExist(err), "Expected test.txt to exist")
+	if !os.IsNotExist(err) {
+		assert.NoError(t, err)
+	}
+	
+	if stat != nil {
+		defer os.Remove("test.txt")
+		assert.Equal(t, int64(9), stat.Size())
+	}
+}
