a75b0079359e61462aea6b98178ba81ddb7398ac
Kamil Trzcinski <ayufan@ayufan.eu>
2015-11-30 10:43:46 +0100

    Dump goroutines on SIGUSR1

--- helpers/cli/log_level.go
+++ helpers/cli/log_level.go
@@ -4,6 +4,9 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 )
 
 func SetupLogLevelOptions(app *cli.App) {
@@ -35,6 +38,17 @@ func SetupLogLevelOptions(app *cli.App) {
 		// enforce log-level=debug.
 		if !c.IsSet("log-level") && !c.IsSet("l") && c.Bool("debug") {
 			log.SetLevel(log.DebugLevel)
+
+			// On USR1 dump stacks of all go routines
+			dumpStacks := make(chan os.Signal, 1)
+			signal.Notify(dumpStacks, syscall.SIGUSR1)
+			go func() {
+				for _ = range dumpStacks {
+					buf := make([]byte, 1<<20)
+					runtime.Stack(buf, true)
+					log.Printf("=== received SIGUSR1 ===\n*** goroutine dump...\n%s\n*** end\n", buf)
+				}
+			}()
 		}
 
 		if appBefore != nil {
