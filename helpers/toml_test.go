7122c5ab7f12445b1a2f4796ce20819e9afa209c
Kamil Trzcinski <ayufan@ayufan.eu>
2016-02-07 00:37:48 +0100

    Fix TOML decoder `omit zero` and `omit empty` problem

---
+++ helpers/toml_test.go
@@ -0,0 +1,21 @@
+package helpers
+
+import (
+	"testing"
+	"github.com/BurntSushi/toml"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTOMLOmitEmpty(t *testing.T) {
+	var config struct {
+		Value int `toml:"value,omitzero"`
+	}
+
+	// This test is intended to test this not fixed problem:
+	// https://github.com/chowey/toml/commit/8249b7bc958927e7a8b392f66adbe4d5ead737d9
+	text := `Value=10`
+	_, err := toml.Decode(text, &config)
+	require.NoError(t, err)
+	assert.Equal(t, 10, config.Value)
+}
