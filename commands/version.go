a2250f2b0a972e2c2b5b54d8efbcc14acd32fe6a
Tomasz Maczukin <tomasz@maczukin.pl>
2016-04-29 13:16:20 +0200

    Add a command with extended version output

---
+++ commands/version.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/codegangsta/cli"
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/common"
+)
+
+type VersionCommand struct {
+	configOptions
+}
+
+func (c *VersionCommand) Execute(context *cli.Context) {
+	fmt.Printf("Version:      %s\n", common.VERSION)
+	fmt.Printf("Git revision: %s\n", common.REVISION)
+	fmt.Printf("GO version:   %s\n", runtime.Version())
+	fmt.Printf("Built:        %s\n", common.BUILT)
+	fmt.Printf("OS/Arch:      %s/%s\n", runtime.GOOS, runtime.GOARCH)
+}
+
+func init() {
+	common.RegisterCommand2("version", "Print version details", &VersionCommand{})
+}
--- common/version.go
+++ common/version.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"time"
+)
+
 var NAME = "gitlab-ci-multi-runner"
 var VERSION = "dev"
 var REVISION = "HEAD"
+var BUILT = time.Now().Format(time.RFC1123Z)
