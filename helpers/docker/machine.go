3e1b2672e9431cd0c85f8415a580851150707de5
Kamil Trzcinski <ayufan@ayufan.eu>
2016-05-05 11:11:30 +0200

    Skip removal of machine that doesn't exist

--- helpers/docker/machine.go
+++ helpers/docker/machine.go
@@ -5,6 +5,7 @@ type Machine interface {
 	Provision(name string) error
 	Remove(name string) error
 	List(nodeFilter string) (machines []string, err error)
+	Exist(name string) bool
 
 	CanConnect(name string) bool
 	Credentials(name string) (DockerCredentials, error)
