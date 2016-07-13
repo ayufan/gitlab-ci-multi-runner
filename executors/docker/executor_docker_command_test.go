b94749b8090622e5e01885705e8b91ff37bd0dde
Kamil Trzcinski <ayufan@ayufan.eu>
2016-07-14 00:52:41 +0200

    Added simple docker executor integration tests

---
+++ executors/docker/executor_docker_command_test.go
@@ -0,0 +1,198 @@
+package docker_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/common"
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/helpers"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+)
+
+func SkipIfNoDocker(t *testing.T) bool {
+	cmd := exec.Command("docker", "info")
+	if cmd.Run() != nil {
+		t.Log("'docker info' failed")
+		t.Skip()
+		return true
+	}
+	return false
+}
+
+func TestDockerCommandSuccessRun(t *testing.T) {
+	if helpers.SkipIntegrationTests(t) {
+		return
+	}
+	if SkipIfNoDocker(t) {
+		return
+	}
+
+	build := &common.Build{
+		GetBuildResponse: common.SuccessfulBuild,
+		Runner: &common.RunnerConfig{
+			RunnerSettings: common.RunnerSettings{
+				Executor: "docker",
+				Docker: &common.DockerConfig{
+					Image: "alpine",
+				},
+			},
+		},
+	}
+
+	err := build.Run(&common.Config{}, &common.Trace{Writer: os.Stdout})
+	assert.NoError(t, err)
+}
+
+func TestDockerCommandBuildFail(t *testing.T) {
+	if helpers.SkipIntegrationTests(t) {
+		return
+	}
+	if SkipIfNoDocker(t) {
+		return
+	}
+
+	build := &common.Build{
+		GetBuildResponse: common.FailedBuild,
+		Runner: &common.RunnerConfig{
+			RunnerSettings: common.RunnerSettings{
+				Executor: "docker",
+				Docker: &common.DockerConfig{
+					Image: "alpine",
+				},
+			},
+		},
+	}
+
+	err := build.Run(&common.Config{}, &common.Trace{Writer: os.Stdout})
+	require.Error(t, err, "error")
+	assert.IsType(t, err, &common.BuildError{})
+	assert.Contains(t, "exit code 1", err.Error())
+}
+
+func TestDockerCommandMissingImage(t *testing.T) {
+	if helpers.SkipIntegrationTests(t) {
+		return
+	}
+	if SkipIfNoDocker(t) {
+		return
+	}
+
+	build := &common.Build{
+		Runner: &common.RunnerConfig{
+			RunnerSettings: common.RunnerSettings{
+				Executor: "docker",
+				Docker: &common.DockerConfig{
+					Image: "some/non-existing/image",
+				},
+			},
+		},
+	}
+
+	err := build.Run(&common.Config{}, &common.Trace{Writer: os.Stdout})
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "image some/non-existing/image:latest not found")
+}
+
+func TestDockerCommandBuildAbort(t *testing.T) {
+	if helpers.SkipIntegrationTests(t) {
+		return
+	}
+
+	build := &common.Build{
+		GetBuildResponse: common.LongRunningBuild,
+		Runner: &common.RunnerConfig{
+			RunnerSettings: common.RunnerSettings{
+				Executor: "docker",
+				Docker: &common.DockerConfig{
+					Image: "alpine",
+				},
+			},
+		},
+		BuildAbort: make(chan os.Signal, 1),
+	}
+
+	abortTimer := time.AfterFunc(time.Second, func() {
+		t.Log("Interrupt")
+		build.BuildAbort <- os.Interrupt
+	})
+	defer abortTimer.Stop()
+
+	timeoutTimer := time.AfterFunc(time.Minute, func() {
+		t.Log("Timedout")
+		t.FailNow()
+	})
+	defer timeoutTimer.Stop()
+
+	err := build.Run(&common.Config{}, &common.Trace{Writer: os.Stdout})
+	assert.EqualError(t, err, "aborted: interrupt")
+}
+
+func TestDockerCommandBuildCancel(t *testing.T) {
+	if helpers.SkipIntegrationTests(t) {
+		return
+	}
+
+	build := &common.Build{
+		GetBuildResponse: common.LongRunningBuild,
+		Runner: &common.RunnerConfig{
+			RunnerSettings: common.RunnerSettings{
+				Executor: "docker",
+				Docker: &common.DockerConfig{
+					Image: "alpine",
+				},
+			},
+		},
+	}
+
+	trace := &common.Trace{Writer: os.Stdout}
+
+	abortTimer := time.AfterFunc(time.Second, func() {
+		t.Log("Interrupt")
+		for trace.Abort == nil {
+			time.Sleep(time.Second)
+		}
+		trace.Abort()
+	})
+	defer abortTimer.Stop()
+
+	timeoutTimer := time.AfterFunc(time.Minute, func() {
+		t.Log("Timedout")
+		t.FailNow()
+	})
+	defer timeoutTimer.Stop()
+
+	err := build.Run(&common.Config{}, trace)
+	assert.IsType(t, err, &common.BuildError{})
+	assert.EqualError(t, err, "canceled")
+}
+
+func TestDockerCommandPrivilegedServices(t *testing.T) {
+	if helpers.SkipIntegrationTests(t) {
+		return
+	}
+
+	build := &common.Build{
+		GetBuildResponse: common.LongRunningBuild,
+		Runner: &common.RunnerConfig{
+			RunnerSettings: common.RunnerSettings{
+				Executor: "docker",
+				Docker: &common.DockerConfig{
+					Image:      "alpine",
+					Privileged: true,
+				},
+			},
+		},
+	}
+	build.Commands = "docker info"
+	build.Options = common.BuildOptions{
+		"image": "docker:git",
+		"services": []string{
+			"docker:dind",
+		},
+	}
+
+	err := build.Run(&common.Config{}, &common.Trace{Writer: os.Stdout})
+	assert.NoError(t, err)
+}
