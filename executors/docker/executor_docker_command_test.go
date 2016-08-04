4a4888271cdb818646b11261bacd90932300113a
Kamil Trzcinski <ayufan@ayufan.eu>
2016-08-04 15:25:07 +0200

    Use .xz for prebuilt docker images
    This allows to reduce the binary size by about 20MB. The change also makes the archive to be decompressed by Docker Engine, not by runner reducing the CPU and Network usage when starting a new build.
    
    This also introduces compatibility tests up to docker 1.8 to make sure that all features do work as expected. Possibly we should make 1.8 a minimal requirement for GitLab Runner.

--- executors/docker/executor_docker_command_test.go
+++ executors/docker/executor_docker_command_test.go
@@ -4,11 +4,16 @@ import (
 	"os"
 	"testing"
 	"time"
+	"os/exec"
+	"strings"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/common"
 	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/helpers"
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/helpers/docker"
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/executors/docker"
 )
 
 func TestDockerCommandSuccessRun(t *testing.T) {
@@ -173,4 +178,131 @@ func TestDockerCommandPrivilegedServices(t *testing.T) {
 
 	err := build.Run(&common.Config{}, &common.Trace{Writer: os.Stdout})
 	assert.NoError(t, err)
+}
+
+func runDockerInDocker(version string) (id string, err error) {
+	cmd := exec.Command("docker", "run", "--detach", "--privileged", "-p", "2375", "docker:" + version + "-dind")
+	cmd.Stderr = os.Stderr
+	data, err := cmd.Output()
+	if err != nil {
+		return
+	}
+	id = strings.TrimSpace(string(data))
+	return
+}
+
+func getDockerCredentials(id string) (credentials docker_helpers.DockerCredentials, err error) {
+	cmd := exec.Command("docker", "port", id, "2375")
+	cmd.Stderr = os.Stderr
+	data, err := cmd.Output()
+	if err != nil {
+		return
+	}
+
+	hostPort := strings.Split(strings.TrimSpace(string(data)), ":")
+	if hostPort[0] == "0.0.0.0" {
+		hostPort[0] = "localhost"
+	}
+	credentials.Host = "tcp://" + hostPort[0] + ":" + hostPort[1]
+	return
+}
+
+func waitForDocker(credentials docker_helpers.DockerCredentials) error {
+	client, err := docker_helpers.New(credentials, docker.DockerAPIVersion)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < 20; i++ {
+		_, err = client.Info()
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	return err
+}
+
+func testDockerVersion(t *testing.T, version string) {
+	t.Log("Running docker", version, "...")
+	id, err := runDockerInDocker(version)
+	if err != nil {
+		t.Error("Docker run:", err)
+		return
+	}
+
+	defer func() {
+		exec.Command("docker", "rm", "-f", "-v", id).Run()
+	}()
+
+	t.Log("Getting address of", version, "...")
+	credentials, err := getDockerCredentials(id)
+	if err != nil {
+		t.Error("Docker credentials:", err)
+		return
+	}
+
+	err = waitForDocker(credentials)
+	if err != nil {
+		t.Error("Wait for docker:", err)
+		return
+	}
+
+	t.Log("Docker", version, "is running at", credentials.Host)
+
+	build := &common.Build{
+		GetBuildResponse: common.SuccessfulBuild,
+		Runner: &common.RunnerConfig{
+			RunnerSettings: common.RunnerSettings{
+				Executor: "docker",
+				Docker: &common.DockerConfig{
+					Image: "alpine",
+					DockerCredentials: credentials,
+				},
+			},
+		},
+	}
+
+	err = build.Run(&common.Config{}, &common.Trace{Writer: os.Stdout})
+	assert.NoError(t, err)
+}
+
+func TestDocker1_8Compatibility(t *testing.T) {
+	if helpers.SkipIntegrationTests(t, "docker", "info") {
+		return
+	}
+
+	testDockerVersion(t, "1.8")
+}
+
+func TestDocker1_9Compatibility(t *testing.T) {
+	if helpers.SkipIntegrationTests(t, "docker", "info") {
+		return
+	}
+
+	testDockerVersion(t, "1.9")
+}
+
+func TestDocker1_10Compatibility(t *testing.T) {
+	if helpers.SkipIntegrationTests(t, "docker", "info") {
+		return
+	}
+
+	testDockerVersion(t, "1.10")
+}
+
+func TestDocker1_11Compatibility(t *testing.T) {
+	if helpers.SkipIntegrationTests(t, "docker", "info") {
+		return
+	}
+
+	testDockerVersion(t, "1.12")
+}
+
+func TestDocker1_12Compatibility(t *testing.T) {
+	if helpers.SkipIntegrationTests(t, "docker", "info") {
+		return
+	}
+
+	testDockerVersion(t, "1.12")
 }
