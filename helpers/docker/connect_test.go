2e36ec6c3f43fa4ab9b845684f9adfa03c46f245
Kamil Trzcinski <ayufan@ayufan.eu>
2016-08-22 14:07:10 +0200

    Fix docker client caching

--- helpers/docker/connect_test.go
+++ helpers/docker/connect_test.go
@@ -9,26 +9,6 @@ import (
 	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/helpers/docker"
 )
 
-func TestCachesClient(t *testing.T) {
-	client1, err := docker_helpers.New(docker_helpers.DockerCredentials{}, "1.0")
-	require.NoError(t, err)
-
-	client2, err := docker_helpers.New(docker_helpers.DockerCredentials{}, "1.0")
-	require.NoError(t, err)
-
-	assert.Equal(t, client1, client2)
-}
-
-func TestNoCacheForDifferentCredentials(t *testing.T) {
-	client1, err := docker_helpers.New(docker_helpers.DockerCredentials{}, "1.1")
-	require.NoError(t, err)
-
-	client2, err := docker_helpers.New(docker_helpers.DockerCredentials{Host: "google.com"}, "1.1")
-	require.NoError(t, err)
-
-	assert.NotEqual(t, client1, client2)
-}
-
 func TestCacheUserUnixSockets(t *testing.T) {
 	dc := docker_helpers.DockerCredentials{Host: "unix://google.com/"}
 
